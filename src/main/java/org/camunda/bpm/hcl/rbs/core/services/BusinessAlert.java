980443c22a6c70c870766291f38727a5082b3c6c
Chandran Muthu <m.chandran200@gmail.com>
2019-03-02 02:25:26 -0800

    update

--- src/main/java/org/camunda/bpm/hcl/rbs/core/services/BusinessAlert.java
+++ src/main/java/org/camunda/bpm/hcl/rbs/core/services/BusinessAlert.java
@@ -1,4 +1,4 @@
-package org.camunda.bpm.hcl.rbs;
+package org.camunda.bpm.hcl.rbs.core.services;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Component;
 
 
 @Component("businessAlert")
-public class BusinessAlert implements JavaDelegate{
+public class BusinessAlert implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution arg0) throws Exception {
+		// TODO Auto-generated method stub
 		System.out.println("Business Alert Triggered");
 	}
 
